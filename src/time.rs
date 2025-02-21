de3c7fdb063a6b321aae747a4928e90731c2435f
ibaryshnikov <baryshnikov.il@gmail.com>
2025-02-21 17:35:17 +0300

    use unsigned long long in ffi

--- src/time.rs
+++ src/time.rs
@@ -1,28 +1,28 @@
-use std::ffi::c_ulong;
+use std::ffi::c_ulonglong;
 use std::time::Duration;
 
 use crate::RawFuture;
 
 #[no_mangle]
-extern "C" fn duration_from_secs(value: c_ulong) -> *mut Duration {
+extern "C" fn duration_from_secs(value: c_ulonglong) -> *mut Duration {
     let duration = Duration::from_secs(value);
     Box::into_raw(Box::new(duration))
 }
 
 #[no_mangle]
-extern "C" fn duration_from_millis(value: c_ulong) -> *mut Duration {
+extern "C" fn duration_from_millis(value: c_ulonglong) -> *mut Duration {
     let duration = Duration::from_millis(value);
     Box::into_raw(Box::new(duration))
 }
 
 #[no_mangle]
-extern "C" fn duration_from_micros(value: c_ulong) -> *mut Duration {
+extern "C" fn duration_from_micros(value: c_ulonglong) -> *mut Duration {
     let duration = Duration::from_micros(value);
     Box::into_raw(Box::new(duration))
 }
 
 #[no_mangle]
-extern "C" fn duration_from_nanos(value: c_ulong) -> *mut Duration {
+extern "C" fn duration_from_nanos(value: c_ulonglong) -> *mut Duration {
     let duration = Duration::from_nanos(value);
     Box::into_raw(Box::new(duration))
 }
